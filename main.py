ef7067ac2a70fe496739bcf5d6d08738c194d9c6
hoyohai <yolitme0@gmail.com>
2021-11-18 12:16:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ number = "40200303210349"
 colour = "yes"
 activity = "waste time"
 print("Hello! My name is " + n + ". " + "My lucky number is " + number + ". " + "My favourite colour is " + colour + " and something I like to do is " + activity + ".")
+iasdfiasdi = 12398712937

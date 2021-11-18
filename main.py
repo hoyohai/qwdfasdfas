46e713bff46d5e133bb9430282a8d6e04c471b01
WesleyZhang2 <>
2021-11-18 19:09:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+n = "bendan"
+number = "40200303210349"
+colour = "yes"
+activity = "waste time"
+print("Hello! My name is " + n + ". " + "My lucky number is " + number + ". " + "My favourite colour is " + colour + " and something I like to do is " + activity + ".")
